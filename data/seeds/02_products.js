edcff68cef2187a319cfa858af0add2cca3c62f0
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-04-30 16:05:55 -0400

    added userid

--- data/seeds/02_products.js
+++ data/seeds/02_products.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('products').insert([
         {id: 1, name: 'Animal Products', market_location: 'farm', description: 'Livestock', quantity: '3', price: '50', user_id: 1},
-        {id: 2, name: 'Beans', market_location: 'farm', description: 'Beans', quantity: '10', price: '15', user_id: '1'},
+        {id: 2, name: 'Beans', market_location: 'farm', description: 'Beans', quantity: '10', price: '15', user_id: 1},
         {id: 3, name: 'Fruits', market_location: 'orchard', description: 'Mangoes', quantity: '7', price: '23', user_id: 1},
         {id: 4, name: 'Rug', market_location: 'Bizaar', description: 'newly woven carpet!', quantity: '3', price: '50', user_id: 1},
         {id: 5, name: 'Loafers', market_location: 'Bizaar', description: 'very sturdy for working!', quantity: '12', price: '15', user_id: 1},
