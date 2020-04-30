861b9b23c449021114c510a488a73a4c77866e11
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-04-30 13:03:18 -0400

    added some fields/dummy data

--- users/users-router.js
+++ users/users-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Users = require("./users-model.js");
+const Products = require("../products/products-model");
 
 router.get("/", (req, res) => {
     const requestOptions = {
@@ -14,5 +15,45 @@ router.get("/", (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Users.find(id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+
+    .catch(err => {
+      res.status(500).json({ error: 'user not received' })
+    })
+})
+
+router.get('/:id/products', (req, res) => {
+  console.log(req.params.id);
+  Products.findProductByUser(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not get the list of products for user' })
+    })
+})
+
+
+
+router.post('/:id/products', (req, res) => {
+  const id = req.params.id;
+  let products = req.body;
+  products = { ...products, user_id: id };
+  Products.add(products)
+    .then(newProduct => {
+      res.status(201).json(newProduct);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Could not save the product' });
+
+    })
+
+})
 
 module.exports = router;
